f8160111f6854f2a9727edbd9ef15a9396406f22
MW-Net-Core <walizia24@gmail.com>
2021-10-19 11:57:54 -0700

    Add project files.

---
+++ PracHome/Data/DataContext.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracHome.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracHome.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> TblStudents { get; set; }
+        public DbSet<Standard> TblStandards { get; set; }
+        public DbSet<StudentStandardRelation> StudentStandardRelations { get; set; }
+
+
+        //public DbSet<User_Model> Tbl_User { get; set; }
+        //public DbSet<Role_Model> Tbl_Role { get; set; }
+        // public DbSet<User_Role_Status> Tbl_URS_JOIN { get; set; }
+
+
+        /*
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //primary keys
+                //modelBuilder.Entity<User_Role_Status>().HasKey(o => o.URS_PK_Id);
+                //modelBuilder.Entity<User_Model>().HasKey(o => o.User_Id);
+                //modelBuilder.Entity<Role_Model>().HasKey(o => o.Role_Id);
+                //modelBuilder.Entity<Status_Model>().HasKey(o => o.Status_Id);
+            //primary key
+
+            //user model columns validation
+            //modelBuilder.Entity<User_Model>().Property(t => t.User_Name).IsRequired().HasMaxLength(50);
+            //modelBuilder.Entity<User_Model>().HasIndex(t => t.User_Name).IsUnique();
+            //modelBuilder.Entity<User_Model>().Property(t => t.User_Password).IsRequired();
+            //user model columns validation
+
+            //role model columns validation
+            //modelBuilder.Entity<Role_Model>().Property(t => t.Role_Name).IsRequired().HasMaxLength(50);
+            //modelBuilder.Entity<Role_Model>().HasIndex(t => t.Role_Name).IsUnique();
+            //role model columns validation
+
+            /*
+             *Many Users can have many roles [all the configuration is here]
+              many-user -> many-roles third table is urs
+             *
+             */
+
+            //modelBuilder.Entity<User_Role_Status>()
+            //    .HasKey(T => new { T.User_Id_FK, T.Role_Id_FK });
+            //modelBuilder.Entity<User_Role_Status>()
+            //    .HasOne(user => user.User_Obj).WithMany(urs => urs.URS_Objs).HasForeignKey(FK_User => FK_User.User_Id_FK);
+            //modelBuilder.Entity<User_Role_Status>()
+            //    .HasOne(role => role.Role_Obj).WithMany(urs => urs.URS_Objs).HasForeignKey(FK_User => FK_User.Role_Id_FK);
+
+            /*
+             *Many Users can have many roles [all the configuration is here]
+              many-user -> many-roles third table is urs and configured above
+             *
+            
+           
+
+        }
+    */
+
+    }
+}
---
+++ PracHome/Models/User_Model.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracHome.Models
+{
+    //public class User_Model
+    //{
+    //    [Key]
+    //    public Guid User_Id { get; set; }
+    //    public string User_Name { get; set; }
+    //    public string User_Password { get; set; }
+    //    public IEnumerable<User_Role_Status> URS_Objs { get; set; }
+    //}
+
+    public class Standard
+    {
+        public int StandardId { get; set; }
+        public string StandardName { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
